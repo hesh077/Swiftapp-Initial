b04d7933faee5e70ce1fa4ab4fdf7433b0cc90fd
Mahesh Mudradi <hesh077@maheshs-macbook-pro.local>
2017-07-24 23:11:47 -0500

    Initial Commit

---
+++ Xocde Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xocde Project
+//
+//  Created by Mahesh Mudradi on 7/24/17.
+//  Copyright © 2017 Mahesh Mudradi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
